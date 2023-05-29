df51b2b807195c66c153ba7f069a702af26eb3de
Kolomiiets Lidiia <kolomieclidia7@gmail.com>
2023-05-29 18:53:09 +0300

    fix comments

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const manyHours = prompt("How many hours?");
 let manyHoursNumber = null
 
-if (manyHours.trim()){
+if (manyHours && manyHours.trim()){
     manyHoursNumber = +manyHours;
 }
 
 if(manyHoursNumber > 0 || manyHoursNumber === 0){
-   let result = manyHoursNumber*60*60;
+   const result = manyHoursNumber*60*60;
     alert(result + " seconds!");
 }
 
